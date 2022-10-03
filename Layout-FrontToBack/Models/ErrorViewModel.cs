be1f6f57b20e18f14994ac6a1098a759dfa66e0d
fidanmardanli <fidannm@code.edu.az>
2022-10-04 01:00:06 +0400

    task

---
+++ Layout-FrontToBack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Layout_FrontToBack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
